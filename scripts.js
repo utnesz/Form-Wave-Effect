5c4551648782b1fc321f161702295e0d038427b6
utnesz <90829509+utnesz@users.noreply.github.com>
2023-01-09 10:16:28 +0100

    Done

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const labels = document.querySelectorAll(".form-control label")
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
-    .split('')
-    .map((letter, i)=>`<span>${letter}</span>`)
-    .join('')
+        .split('')
+        .map((letter, i) => `<span style="transition-delay:${i * 20}ms">${letter}</span>`)
+        .join('')
 })
